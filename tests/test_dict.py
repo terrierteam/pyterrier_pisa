5aaacf0f86bcfe3ec187f368f2eff137ce696977
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2022-11-17 13:59:54 +0000

    testing using vaswani

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -15,13 +15,31 @@ class DictTest(TempDirTestCase):
   #       import pdb; pdb.set_trace()
   #       pass
 
+    def test_vaswani(self):
+      from pyterrier_pisa import PisaIndex, DictTokeniser
+      dataset = pt.get_dataset('irds:msmarco-passage')
+      idx = PisaIndex(self.test_dir+'/index', text_field='text_dict', pretokenised=True, stemmer='none')
+      idx = DictTokeniser() >> idx
+      idx.index(dataset.get_corpus_iter())
+      self.assertTrue(idx.built())
+      self.assertEqual(len(idx), 11429)
+      MAPS=[0.300]
+      TRANS=[idx.bm25()]
+      exp_df = pt.Experiment(
+          TRANS,
+          *dataset.get_topicsqrels(),
+          ["map"],
+          round=6
+      )
+      self.assertListEqual(MAPS, exp_df["map"].tolist())
+
     def test_dict(self):
-        from pyterrier_pisa import PisaIndex, DictTokeniser
-        dataset = pt.get_dataset('irds:msmarco-passage')
+        from pyterrier_pisa import PisaIndex
         idx = PisaIndex(self.test_dir+'/index', text_field='text_dict', pretokenised=True, stemmer='none')
         idx.index([
           {'docno' : 'd1', 'text_dict' : {'a' : 1, 'b' : 14}}
         ])
+        self.assertTrue(idx.built())
         #import pdb; pdb.set_trace()
         #pass
 
