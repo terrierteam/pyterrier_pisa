1bc8815e77e495d2d21986ae1663953602acd6b6
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2022-11-17 12:49:30 +0000

    index a dictionary

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -5,15 +5,25 @@ import itertools
 
 class DictTest(TempDirTestCase):
 
+  # def test_dict(self):
+  #       from pyterrier_pisa import PisaIndex, DictTokeniser
+  #       dataset = pt.get_dataset('irds:msmarco-passage')
+  #       idx = PisaIndex(self.test_dir+'/index', text_field='text_dict', stemmer='none')
+  #       #(DictTokeniser() >> idx).index(itertools.islice(dataset.get_corpus_iter(), 200000))
+  #       #(DictTokeniser() >> idx).index(dataset.get_corpus_iter())
+  #       idx.index(PisaIndex('/home/sean/data/indices/msmarco-passage.pisa').get_corpus_iter())
+  #       import pdb; pdb.set_trace()
+  #       pass
+
     def test_dict(self):
         from pyterrier_pisa import PisaIndex, DictTokeniser
         dataset = pt.get_dataset('irds:msmarco-passage')
-        idx = PisaIndex(self.test_dir+'/index', text_field='text_dict', stemmer='none')
-        #(DictTokeniser() >> idx).index(itertools.islice(dataset.get_corpus_iter(), 200000))
-        #(DictTokeniser() >> idx).index(dataset.get_corpus_iter())
-        idx.index(PisaIndex('/home/sean/data/indices/msmarco-passage.pisa').get_corpus_iter())
-        import pdb; pdb.set_trace()
-        pass
+        idx = PisaIndex(self.test_dir+'/index', text_field='text_dict', pretokenised=True, stemmer='none')
+        idx.index([
+          {'docno' : 'd1', 'text_dict' : {'a' : 1, 'b' : 14}}
+        ])
+        #import pdb; pdb.set_trace()
+        #pass
 
 if __name__ == "__main__":
   import unittest
