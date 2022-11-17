2a7d92981ab866488cff07b63e81f44354471001
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2022-11-17 12:35:44 +0000

    remove duplicated method

--- src/pyterrier_pisa/_pisathon.cpp
+++ src/pyterrier_pisa/_pisathon.cpp
@@ -267,24 +267,6 @@ static PyObject *py_build_binlex(PyObject *self, PyObject *args, PyObject *kwarg
   Py_RETURN_NONE;
 }
 
-static PyObject *py_build_binlex(PyObject *self, PyObject *args, PyObject *kwargs) {
-  const char* term_file;
-  const char* termlex_file;
-
-  /* Parse arguments */
-  if(!PyArg_ParseTuple(args, "ss", &term_file, &termlex_file)) {
-      return NULL;
-  }
-
-  std::string s_term_file(term_file);
-  std::string s_termlex_file(termlex_file);
-
-  std::ifstream is(s_term_file);
-  encode_payload_vector(std::istream_iterator<io::Line>(is), std::istream_iterator<io::Line>()).to_file(s_termlex_file);
-
-  Py_RETURN_NONE;
-}
-
 
 
 
