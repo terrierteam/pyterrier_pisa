3c26483c5307eb83641cb32048cd77f1b3ff50a3
Sean MacAvaney <sean.macavaney@gmail.com>
2025-06-15 22:36:34 +0100

    bump version

--- src/pyterrier_pisa/__init__.py
+++ src/pyterrier_pisa/__init__.py
@@ -19,7 +19,7 @@ from . import _pisathon
 from .indexers import PisaIndexer, PisaToksIndexer, PisaIndexingMode
 from .stopwords import _STOPWORDS
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 _logger = ir_datasets.log.easy()
 
