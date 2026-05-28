8487d529f7316d9e6e3631ee7a24b03072e8f1b8
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2026-05-28 17:57:30 +0100

    Improve LD_LIBRARY_PATH handling for tbb
    Refactor LD_LIBRARY_PATH setting for tbb library location.

--- src/pyterrier_pisa/__init__.py
+++ src/pyterrier_pisa/__init__.py
@@ -19,9 +19,11 @@ import ir_datasets
 # add tbb location to library path
 from importlib.metadata import distribution
 lib_path = str(distribution("tbb")._path) + "/../../../"
-os.environ["LD_LIBRARY_PATH"] = (
-    os.environ.get("LD_LIBRARY_PATH", "") + ":" if "LD_LIBRARY_PATH" in os.environ else ""
-) + lib_path
+if "LD_LIBRARY_PATH" in os.environ:
+    if lib_path not in os.environ["LD_LIBRARY_PATH"]:
+        os.environ["LD_LIBRARY_PATH"] += ":" + path
+else:
+    os.environ["LD_LIBRARY_PATH"] = lib_path
 from . import _pisathon
 
 
